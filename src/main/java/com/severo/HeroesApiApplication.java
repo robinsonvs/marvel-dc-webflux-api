bc9849482156f8420cd97ada7300025379a87e7c
robinsonvs <robinsonvs@gmail.com>
2021-04-05 01:02:58 -0300

    update

--- src/main/java/com/severo/HeroesApiApplication.java
+++ src/main/java/com/severo/HeroesApiApplication.java
@@ -1,4 +1,4 @@
-package com.digitalinnovation.livecoding;
+package com.severo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
